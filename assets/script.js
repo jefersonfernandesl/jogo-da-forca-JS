a4baa2550fb2919a70e702bec8ef135c5a642e00
Jeferson Fernandes <dev.jefersonl@gmail.com>
2024-02-22 08:50:02 -0300

    correção na lógica

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-const bancoDePalavras = ["monitor", "mouse", "computador", "notebook", "teclado"];
+const bancoDePalavras = ["monitor", "mouse", "computador", "notebook", "teclado", "banana", "carro", "computador", "cachorro", "lua", "montanha", "foguete", "praia", "livro", "chocolate", "aviao", "telefone", "jardim", "sorvete", "girassol", "elefante", "casa", "chave", "cafe", "bicicleta", "arvore", "rio", "lapis", "sol", "terra", "vento", "navio", "mochila", "cadeira", "amigo", "paisagem", "cidade", "ocean", "teclado", "gato", "escola", "papel", "radio", "bolacha", "flor", "espelho", "pintura", "esquilo", "abacaxi", "dinossauro", "inverno", "primavera", "verao", "outono", "neve", "chuva", "estrela", "rocha", "caverna", "sonho", "viagem", "feira", "mercado", "hamburguer", "pizza", "piscina", "festa", "caminhada", "travesseiro", "cobertor", "cobertor", "relogio", "luz", "caneta", "filme", "foto", "batata", "melancia", "limao", "laranja", "banana", "mel", "abelha", "formiga", "mariposa", "nuvem", "ventilador", "porta", "janela", "cachoeira", "sabao", "escova", "faca", "garfo", "colher"];
 
 function inciarJogo() {
+
     let palavraSorteada = sortearPalavraDoBanco().toUpperCase().split('');
     let tentativas = 6;
     let letrasChutadas = [];
@@ -8,23 +9,19 @@ function inciarJogo() {
     for (let i = 0; i < palavraSorteada.length; i++) {
         palavraJogador.push('_');
     }
-
     atualizarPlacar();
-    const teclas = document.querySelectorAll(".btn")
+    const teclas = document.querySelectorAll(".btn");
 
     teclas.forEach(tecla => {
         tecla.addEventListener("click", function () {
-            if (tentativas <= 0) {
-
-            }
-            verificarLetra(tecla.value)
+            verificarLetra(tecla.value);
         })
     })
 
     function atualizarPlacar() {
         if (tentativas <= 0) {
-            document.getElementById("numberAttempts").innerHTML = "Você perdeu!";
-            let palavraExibicao = document.getElementById("wordLine").innerHTML = palavraJogador.toString();
+            document.getElementById("numberAttempts").innerHTML = "Você perdeu! A palavra era:";
+            let palavraExibicao = document.getElementById("wordLine").innerHTML = palavraSorteada.toString();
             document.getElementById("wordLine").innerHTML = palavraExibicao.replaceAll(",", " ");
 
         } else {
@@ -36,11 +33,16 @@ function inciarJogo() {
     }
 
     function verificarSeAcertouAPalavra() {
-        if (palavraJogador.toString === palavraSorteada.toString) {
-            return true
+        console.log(palavraJogador.toString());
+        console.log(palavraSorteada.toString());
+        if (palavraJogador.toString() === palavraSorteada.toString()) {
+            document.getElementById("numberAttempts").innerHTML = `Você ganhou!`;
+            let palavraExibicao = document.getElementById("wordLine").innerHTML = palavraJogador.toString();
+            document.getElementById("wordLine").innerHTML = palavraExibicao.replaceAll(",", " ");
         }
         else {
-            return false
+            console.log('nao sao');
+            atualizarPlacar();
         }
     }
 
@@ -51,29 +53,22 @@ function inciarJogo() {
 
         if (!verificarSeALetraJaFoiUsada == true) {
             letrasChutadas.push(letraChutada);
-
             for (let i = 0; i < palavraSorteada.length; i++) {
                 if (letraChutada == palavraSorteada[i]) {
                     delete palavraJogador[i];
-                    palavraJogador[i] = letraChutada.toUpperCase()
-                    let teste = verificarSeAcertouAPalavra()
-                    if (teste == true) {
-                        document.getElementById("numberAttempts").innerHTML = `Você ganhou!`;
-                        let palavraExibicao = document.getElementById("wordLine").innerHTML = palavraJogador.toString();
-                        document.getElementById("wordLine").innerHTML = palavraExibicao.replaceAll(",", " ");
-                    }
-                    else {
-                        atualizarPlacar();
-                    }
+                    palavraJogador[i] = letraChutada.toUpperCase();
+                    verificarSeAcertouAPalavra();
                 }
                 else {
+                    verificarSeAcertouAPalavra()
+                    //atualizarPlacar();
                     naoAchou.push("nao achou");
-                    atualizarPlacar();
                 }
             }
 
             if (naoAchou.length == palavraSorteada.length) {
                 tentativas -= 1
+                atualizarPlacar();
             }
 
         }
@@ -91,6 +86,5 @@ function inciarJogo() {
     }
 
 }
-//document.getElementById("numberAttempts").innerHTML = `Você perdeu`;
 
 inciarJogo();
